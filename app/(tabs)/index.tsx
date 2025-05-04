7b46c94a30552a9060e4a49e9e5b7380821a609e
nawzneen <earworm90@gmail.com>
2025-05-04 14:57:37 +0300

    feat: wrap HomeScreen component with QueryClientProvider for React Query integration

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,28 @@
 import { AuthPanel } from "@/components/AuthPanel";
 import { ImageCard } from "@/components/ImageCard";
 import { IMAGES } from "@/constants/images";
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useMemo, useState } from "react";
 import { ScrollView, View } from "react-native";
 export default function HomeScreen() {
+  const qc = useMemo(() => new QueryClient(), []);
   const [userId, setUserId] = useState<string | null>(null);
   return (
-    <ScrollView className="p-4">
-      <View className="items-center my-8">
-        <AuthPanel
-          userId={userId}
-          onSignIn={(id) => setUserId(id)}
-          onSignOut={() => setUserId(null)}
-        />
-      </View>
-      <View className=" flex flex-row justify-center items-center gap-x-2">
-        {IMAGES.map((img) => (
-          <ImageCard key={img.id} image={img} userId={userId ?? undefined} />
-        ))}
-      </View>
-    </ScrollView>
+    <QueryClientProvider client={qc}>
+      <ScrollView className="p-4">
+        <View className="items-center my-4">
+          <AuthPanel
+            userId={userId}
+            onSignIn={(id) => setUserId(id)}
+            onSignOut={() => setUserId(null)}
+          />
+        </View>
+        <View className=" flex flex-row justify-center items-center gap-x-2">
+          {IMAGES.map((img) => (
+            <ImageCard key={img.id} image={img} userId={userId ?? undefined} />
+          ))}
+        </View>
+      </ScrollView>
+    </QueryClientProvider>
   );
 }
